c847b30be0c510322531c12247be55dd512ab964
Jacob Crabtree <Jacob.Crabtree@sckans.edu>
2021-12-02 11:43:47 -0600

    edit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Homepage from './nav-video.svg';
+// import Button from './button.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <img src={Homepage} class="Homepage"></img>
+      {/* <img src={Button} class="Button"></img> */}
     </div>
   );
 }
